3d3b241be897099b511df94d7d9cf906b7705ce1
Andriy <topcoder1406@gmail.com>
2022-09-11 18:22:50 +0300

    fixed redirect on validation error bug

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -39,7 +40,8 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        if ($exception instanceof NotFoundHttpException) {
+        if (($exception instanceof NotFoundHttpException)
+            || ($exception instanceof ValidationException)) {
             return parent::render($request, $exception);
         }
 
